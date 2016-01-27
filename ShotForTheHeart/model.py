40a7b8bdc3d509ebf3b270eb9b986c357fbfcce2
Rick Knoop <knoop.rick@gmail.com>
2016-01-27 12:35:00 -0500

    Creating a custom user model

--- ShotForTheHeart/model.py
+++ ShotForTheHeart/model.py
@@ -1,15 +1,93 @@
+from django.core import validators
 from django.db import models
 from django.contrib.auth.models import User
-from re import search as regex
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.utils import timezone
+import re
 
 
 
 class userInfo (models.Model):
 	user = models.OneToOneField(User, on_delete=models.CASCADE)
-	program = models.CharField(max_length=50)
-	year = models.CharField(max_length=3)
-	hangout = models.CharField(max_length=50)
+	study_program = models.CharField(max_length=50)
+	study_year = models.PositiveSmallIntegerField()
+	hangout_spot = models.CharField(max_length=50)
 	profile_photo = models.ImageField(upload_to='tmp')
+
+	def ChangeProgram(program_post):
+		if len(program_post) < 50:
+			self.study_program = program_post
+			self.save()
+			return True
+		return False
+			
+	def ChangeYear(year_post):
+		if year_post is not None and year_post.isdigit():
+			self.study_year = int(year_post)
+			self.save()
+			return True
+		return False
+	
+	def ChangeHangout(hangout_post):
+		if len(hangout_post) < 50:
+			self.study_hangout = hanout_post
+			self.save()
+			return True
+		return False
+		
+		
+class CustomUser(AbstractBaseUser):
+	user_email = models.CharField(max_length=127, unique=True, validators=[
+		validators.RegexValidator(
+			re.compile("[a-zA-Z0-9_\.\+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-\.]+"),
+			_("Enter a valid email."),
+			"ERROR")
+			])
+	full_name = models.CharField(max_length=126, blank=True)
+	study_program = models.CharField(max_length=50, blank=True)
+	study_year = models.PositiveSmallIntegerField(blank=True)
+	hangout_spot = models.CharField(max_length=50, blank=True)
+	#profile_photo = models.ImageField(upload_to='tmp', blank=True)
+	last_login = models.DateTimeField(null=True, blank=True)
+	user_eliminated = models.BooleanField(default = False)
+	target_id = models.PositiveSmallIntegerField(blank=True)
+	
+	#Required Fields
+	is_staff = models.BooleanField(_('staff status'), default=False, help_text = ("Designates whether the user can login to the admin site")
+	is_active = models.BooleanField(_('active'), default=True, help_text = ("Whether the user is active or not, set to false instead of deleting accounts")
+	date_joined = models.DateTimeField(_("Date joined"), default=timezone.now)
+
+
+	USERNAME_FIELD = 'user_email'
+	
+	def get_full_name(self):
+		return self.full_name
+	
+	def get_short_name(self):
+		return self.full_name
+	
+	def ChangeProgram(self, program_post):
+		if len(program_post) < 50:
+			self.study_program = program_post
+			self.save()
+			return True
+		return False
+			
+	def ChangeYear(self, year_post):
+		if year_post is not None and year_post.isdigit():
+			self.study_year = int(year_post)
+			self.save()
+			return True
+		return False
+	
+	def ChangeHangout(self, hangout_post):
+		if len(hangout_post) < 50:
+			self.study_hangout = hanout_post
+			self.save()
+			return True
+		return False
+	
+	
 
 def authorize(request):
 	email = request.POST.get('Email')
